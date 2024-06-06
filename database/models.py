9ac8ec6c5688fff595384f7db76b4d06e3439be3
Erik <erik.agayan1@gmail.com>
2024-06-06 13:42:48 +0300

    Add models

---
+++ database/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database.engine import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(Integer, unique=True, index=True)
+
+    words = relationship("Word", back_populates="owner")
+
+
+class Word(Base):
+    __tablename__ = "words"
+    id = Column(Integer, primary_key=True, index=True)
+    german = Column(String, index=True)
+    russian = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="words")
